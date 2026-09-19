e36d6f0e08ff23c5548858fbfa467bbe86070830
GR <10486725+gady-rozin@users.noreply.github.com>
2026-09-19 14:37:25 +0300

    Configure WebRootPath and clean up middleware
    Set the WebRootPath to the wwwroot folder in the publish directory and removed redundant default files middleware.

--- src/Portelem/Program.cs
+++ src/Portelem/Program.cs
@@ -1,10 +1,12 @@
 var builder = WebApplication.CreateBuilder(args);
+
+// Explicitly set the WebRootPath to the wwwroot folder in the publish directory
+var publishDir = AppContext.BaseDirectory;
+builder.Environment.WebRootPath = Path.Combine(publishDir, "wwwroot");
+
 var app = builder.Build();
 
-// Enables serving static files (like index.html) from the wwwroot directory
-app.UseStaticFiles();
-
-// Sets index.html as the default fall-back file for root URLs ("/")
 app.UseDefaultFiles();
+app.UseStaticFiles();
 
 app.Run();
